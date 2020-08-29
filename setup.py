d2c3941a527c49f63175b8814128952c0cac565d
Jordi Puiggené <jp@naralabs.com>
2020-08-29 22:49:13 +0200

    Pin senaite.queue 1.0.2

--- setup.py
+++ setup.py
@@ -29,9 +29,7 @@ setup(
     install_requires=[
         "selenium",
         "setuptools",
-        "senaite.lims>=1.3.0",
-        "senaite.lims<2.0.0",
-        "senaite.queue",
+        "senaite.queue>=1.0.2",
     ],
     extras_require={
         "test": [
